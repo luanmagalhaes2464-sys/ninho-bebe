6b87675984c6060b797abceb02b08ad6900584cc
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-06 14:09:35 -0300

    Atualiza cache do card realista

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ninho-v5-login';
+const CACHE = 'ninho-v6-bebe-realista';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
