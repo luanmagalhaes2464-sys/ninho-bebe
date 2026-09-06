9731ec11866b048a1da4b24758606a92d42d6167
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-06 19:15:35 -0300

    Atualiza cache do enxoval completo

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ninho-v9-humanizado-estacoes';
+const CACHE = 'ninho-v10-filtro-pendentes-higiene';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
