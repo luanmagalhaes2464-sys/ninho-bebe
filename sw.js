9ab3f049a76a2d80b5a748f77a80cc9279aa93f7
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-06 18:00:50 -0300

    Atualiza cache do visual humanizado

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ninho-v8-wikimedia-gif';
+const CACHE = 'ninho-v9-humanizado-estacoes';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
