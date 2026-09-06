bf9de449305353676a5589037c923d344b094e38
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-06 19:19:12 -0300

    Atualiza cache do login persistente

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ninho-v10-filtro-pendentes-higiene';
+const CACHE = 'ninho-v11-login-persistente';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './manifest.webmanifest'];
 
 self.addEventListener('install', event => {
