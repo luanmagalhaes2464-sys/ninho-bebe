cc62a00ef9178d4c794ddaa789f463a35b21ca46
luanmagalhaes2464-sys <luanmagalhaes2464@gmail.com>
2026-09-18 17:58:47 -0300

    Atualiza cache da evolução semanal do Ian

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'ninho-v22-medida-ian-evolucao';
+const CACHE = 'ninho-v23-visual-semanal-ian';
 const ASSETS = ['./', './index.html', './styles.css', './app.js', './price-radar.js', './manifest.webmanifest', './mascote-ninho-v2.webp'];
 
 self.addEventListener('install', event => {
